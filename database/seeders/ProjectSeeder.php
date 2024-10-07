1e84a7080bafe00d0e58aa676dcb9769bf1df162
B0G0N1 <michelebogoni2001@gmail.com>
2024-10-07 07:41:10 +0200

    [FEAT] Create Seeder(faker) and Fillable(on model)

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;  // Importa il modello Project
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;  // Importa Str per generare lo slug
 
 class ProjectSeeder extends Seeder
 {
@@ -14,6 +16,19 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 20; $i++) {
+
+            $title = $faker->sentence(3);
+            
+            Project::create([
+                'title' => $title,
+                'description' => $faker->paragraph(5),
+                'url' => $faker->url(),
+                'slug' => Str::slug($title),
+            ]);
+        }
     }
 }
